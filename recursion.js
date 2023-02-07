f83a23f2b9dc5c4d7286afb0ab54b6d90649e639
Charlotte Webster <charlotte.shae.webster@gmail.com>
2023-02-07 18:18:15 -0500

    refactor complete

--- recursion.js
+++ recursion.js
@@ -1,22 +1,25 @@
 /** product: calculate the product of an array of numbers. */
 //[1,2,3,4]
-function product(nums) {
-  if (nums.length === 0) return 1; //base case
-  return nums[0] * product(nums.slice(1)); //progress
+
+function product(nums, i=0) {
+  if (nums.length === i) return 1; //base case
+  return nums[i] * product(nums, i + 1); //progress
 }
 
 /** longest: return the length of the longest word in an array of words. */
 //longest(["hello", "hi", "hola"]);  // 5
-function longest(words) {
-  if (words.length === 0) return 0; //base case
-  return Math.max(words[0].length, longest(words.slice(1))); //progress
+
+function longest(words, i=0) {
+  if (words.length === i) return 0; //base case
+  return Math.max(words[i].length, longest(words, i + 1)); //progress
 }
 
 /** everyOther: return a string with every other letter. */
 //everyOther("hello");  // "hlo"
-function everyOther(str) {
-  if (str.length === 0) return ""; //base case
-  return str[0] + everyOther(str.slice(2)); //progress
+
+function everyOther(str, i=0) {
+  if (str.length <= i) return ""; //base case
+  return str[i] + everyOther(str, i + 2); //progress
 }
 
 /** find: return boolean depending on if val exists in array or not. */
@@ -24,24 +27,26 @@ function everyOther(str) {
 
 // find(animals, "cat");  // true
 // find(animals, "porcupine");   // false
-function find(arr, val) {
-  if (arr.length === 0) return false; //base case
-  return arr[0] === val || find(arr.slice(1), val); //progress
+
+function find(arr, val, i=0) {
+  if (arr.length === i) return false; //base case
+  return arr[i] === val || find(arr, val, i + 1); //progress
 }
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
-function isPalindrome(str) {
-  if (str.length === 0) return true; // base case
-  return str[0] === str[str.length - 1] &&
-    isPalindrome(str.slice(1, str.length - 1)); // progress
+function isPalindrome(str, left=0, right=str.length-1) {
+  if (right < left) return true; //base case
+
+  return str[left] === str[right] &&
+    isPalindrome(str, left + 1, right - 1); // progress
 }
 
 /** revString: return a copy of a string, but in reverse. */
 
-function revString(str) {
-  if (str.length === 0) return ''; // base case
-  return str[str.length - 1] + revString(str.slice(0, str.length - 1)); // progress
+function revString(str, i=str.length-1) {
+  if (i === -1) return ''; // base case
+  return str[i] + revString(str, i - 1); // progress
 }
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
@@ -71,15 +76,17 @@ function gatherStrings(obj, strings=[]) {
 /** binarySearch: given a sorted array of numbers, and a value,
  * return true if val is in array, false if not present). */
 
-function binarySearch(arr, val) {
-  //base case: empty array
-  if (arr.length === 0) return false;
-  const idxToCompare = Math.floor(arr.length / 2);
+function binarySearch(arr, val, left=0, right=arr.length) {
+  if (arr.length === 0) return false; // base case
+  if (left > right) return false; // base case
+
+  const idxToCompare = Math.floor((left + right) / 2);
   if (val === arr[idxToCompare]) return true;
+
   if (val < arr[idxToCompare])  {
-    return binarySearch(arr.slice(0, idxToCompare), val);
+    return binarySearch(arr, val, left, idxToCompare - 1); // progress
   } else {
-    return binarySearch(arr.slice(idxToCompare + 1, arr.length), val);
+    return binarySearch(arr, val, idxToCompare + 1, right); // progress
   }
 }
 
@@ -88,17 +95,15 @@ function binarySearch(arr, val) {
  * return the index of that value (or -1 if val is not present). */
 
 function binarySearchIndex(arr, val, left=0, right=arr.length - 1) {
-  if (arr.length === 0) return -1;
-  if (left > right) return -1;
-  if (left === right) return (arr[left] === val) ? left : -1;
+  if (left > right) return -1; // base case
 
   mid = Math.floor((left + right) / 2);
   if (arr[mid] === val) return mid;
 
   if (arr[mid] > val) {
-    return binarySearchIndex(arr, val, left, mid - 1)
+    return binarySearchIndex(arr, val, left, mid - 1) // progress
   } else {
-    return binarySearchIndex(arr, val, mid + 1, right)
+    return binarySearchIndex(arr, val, mid + 1, right) // progress
   }
 }
 
